f461cf7480817f7d2b70c540b128f208c2c994c0
Sandro-Alessio Gierens <sandro@gierens.de>
2024-08-09 16:21:03 +0200

    feat(client): add Client.loss_sim variable
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/client.rs
+++ src/client.rs
@@ -1,14 +1,17 @@
+use crate::loss_simulation::LossSimulation;
 use std::{net::IpAddr, path::PathBuf};
 
-pub struct Client;
+pub struct Client {
+    loss_sim: Option<LossSimulation>,
+}
 
 impl Client {
-    pub fn new() -> Self {
-        Client
+    pub fn new(loss_sim: Option<LossSimulation>) -> Self {
+        Client { loss_sim }
     }
 
     pub fn run(&self, host: IpAddr, port: u16, files: Vec<PathBuf>) {
-        println!("Client running on {}:{}", host, port);
+        println!("Client querying server on {}:{}", host, port);
         println!("Files to download: {:?}", files);
     }
 }
