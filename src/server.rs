219afbddaf67ce5aaa5e6edbc86679c8fc765886
Sandro-Alessio Gierens <sandro@gierens.de>
2024-08-10 18:25:07 +0200

    feat(server): receive and parse packet
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,6 @@
 use crate::loss_simulation::LossSimulation;
+use crate::wire::Packet;
 use std::net::{Ipv4Addr, SocketAddrV4, UdpSocket};
-use std::str::from_utf8;
 
 pub struct Server {
     port: u16,
@@ -16,7 +16,6 @@ impl Server {
     pub fn run(&self) {
         let socket = UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), self.port))
             .expect("Failed to bind socket");
-        dbg!(&socket);
         let mut buf = [0; 1024];
         loop {
             let size = match socket.recv(&mut buf) {
@@ -26,8 +25,8 @@ impl Server {
                     continue;
                 }
             };
-            let message = from_utf8(&buf[..size]).unwrap();
-            dbg!(message);
+            let packet = Packet::parse_buf(&buf[..size]).expect("Failed to parse packet");
+            dbg!(packet);
         }
     }
 }
