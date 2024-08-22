876f5370d30f2adb7b2ad03f0a40216eccd8ab45
Johannes Pfannschmidt <johannes.pfannschmidt@tum.de>
2024-08-22 17:15:25 +0200

    chore(server): add a few TODOs and switch to tokio socket

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,7 @@
 use crate::loss_simulation::LossSimulation;
 use crate::wire::Packet;
-use std::net::{Ipv4Addr, SocketAddrV4, UdpSocket};
+use std::net::{Ipv4Addr, SocketAddrV4};
+use tokio::net::UdpSocket;
 
 pub struct Server {
     port: u16,
@@ -14,12 +15,23 @@ impl Server {
     }
 
     pub async fn run(&self) -> anyhow::Result<()> {
+        //HashMap for client IPs
+        //TODO: map connectionID -> IP:Port
+
+        //HashMap for connection handlers
+        //TODO: map connectionID -> mpsc::Sender
+
+        //mpsc channel <Packet>: handler output -> transmitter input
+        //TODO
+
         let socket = UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), self.port))
+            .await
             .expect("Failed to bind socket");
         let mut buf = [0; 1024];
 
+        //
         loop {
-            let size = socket.recv(&mut buf)?;
+            let size = socket.recv(&mut buf).await?;
             let packet = Packet::parse_buf(&buf[..size]).expect("Failed to parse packet");
             dbg!(packet);
         }
