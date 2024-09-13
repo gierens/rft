1cc5b3547c3baa4a6f39fa35735bfcc53ca9a1d0
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-13 14:43:30 +0200

    fix(client): add missing timeout import
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/client.rs
+++ src/client.rs
@@ -11,7 +11,7 @@ use std::sync::Arc;
 use std::time::Duration;
 use tokio::net::UdpSocket;
 use tokio::task::spawn_blocking;
-use tokio::time::sleep;
+use tokio::time::{sleep, timeout};
 
 #[derive(Debug)]
 pub struct ClientConfig {
